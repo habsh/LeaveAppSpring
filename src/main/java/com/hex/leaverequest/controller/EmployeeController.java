2285ee1b739724c079f72175c29fad649fd26f63
An Nguyen <tuananvu@gmail.com>
2019-02-25 14:29:09 -0500

    Add repositories, dependencies, properties in pom.xml Add Deployed Resources (WEB-INF / spring4-servlet.xml, web.xml) (for Web App Module facet) Add EmployeeModel, EmployeeDAO, EmployeeService, EmployeeController for Manager Detail module

---
+++ src/main/java/com/hex/leaverequest/controller/EmployeeController.java
@@ -0,0 +1,54 @@
+package com.hex.leaverequest.controller;
+
+import java.util.List;
+
+import com.hex.leaverequest.model.Employee;
+import com.hex.leaverequest.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class EmployeeController {
+	
+	@Autowired
+	EmployeeService employeeService;
+	
+	@RequestMapping(value = "/getAllEmployees", method = RequestMethod.GET, headers = "Accept=application/json")
+	public List<Employee> getEmployees() {
+		
+		List<Employee> listOfEmployees = employeeService.getAllEmployees();
+		return listOfEmployees;
+	}
+
+	@RequestMapping(value = "/getEmployee/{id}", method = RequestMethod.GET, headers = "Accept=application/json")
+	public Employee getEmployeeById(@PathVariable int id) {
+		return employeeService.getEmployee(id);
+	}
+
+	@RequestMapping(value = "/addEmployee", method = RequestMethod.POST, headers = "Accept=application/json")
+	public void addCountry(@RequestBody Employee employee) {	
+		employeeService.addEmployee(employee);
+		
+	}
+
+	@RequestMapping(value = "/updateEmployee", method = RequestMethod.PUT, headers = "Accept=application/json")
+	public void updateEmployee(@RequestBody Employee employee) {
+		employeeService.updateEmployee(employee);
+	}
+
+	@RequestMapping(value = "/deleteEmployee/{id}", method = RequestMethod.DELETE, headers = "Accept=application/json")
+	public void deleteEmployee(@PathVariable("id") int id) {
+		employeeService.deleteEmployee(id);		
+	}	
+	
+	@RequestMapping(value = "/getManager/{empId}", method = RequestMethod.GET, headers = "Accept=application/json")
+	public Employee getManagerByEmpId(@PathVariable int empId) {
+		return employeeService.getManager(empId);
+	}
+}
---
+++ src/main/java/com/hex/leaverequest/model/Employee.java
@@ -0,0 +1,130 @@
+package com.hex.leaverequest.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+import javax.persistence.Id;
+
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import org.hibernate.annotations.Proxy;
+
+
+
+@Entity
+@Table(name = "employee",
+	uniqueConstraints=@UniqueConstraint(columnNames={"emp_id", "emp_mail"}))
+//@NamedQuery(query="from Employee e", name="getEmployee")
+@Proxy(lazy=false)
+public class Employee implements Serializable {
+	public Employee() {
+		super();
+	}
+	
+	public Employee(Integer empId, String empName, Integer empPhone, String empDept, String empMail, Date empDOJ,
+			String leaveBalance, Integer empManagerId) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+		this.empPhone = empPhone;
+		this.empDept = empDept;
+		this.empMail = empMail;
+		this.empDOJ = empDOJ;
+		this.leaveBalance = leaveBalance;
+		this.empManagerId = empManagerId;
+	}
+
+	@Id
+	//@GeneratedValue(generator="increment")
+	@Column(name = "emp_id")
+	Integer empId;
+
+	@Column(name = "emp_name" )	
+	String empName;
+	
+	@Column(name = "emp_phone" )
+	Integer empPhone;
+	
+	@Column(name = "emp_dept" )	
+	String empDept;
+
+	@Column(name = "emp_mail" )		
+	String empMail;
+	
+	@Column(name = "emp_doj" )	
+	Date empDOJ;	
+	
+	@Column(name = "leave_balance" )	
+	String leaveBalance;
+	
+	@Column(name = "emp_mng_id" )	
+	Integer empManagerId;
+
+	public Integer getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public Integer getEmpPhone() {
+		return empPhone;
+	}
+
+	public void setEmpPhone(Integer empPhone) {
+		this.empPhone = empPhone;
+	}
+
+	public String getEmpDept() {
+		return empDept;
+	}
+
+	public void setEmpDept(String empDept) {
+		this.empDept = empDept;
+	}
+
+	public String getEmpMail() {
+		return empMail;
+	}
+
+	public void setEmpMail(String empMail) {
+		this.empMail = empMail;
+	}
+
+	public Date getEmpDOJ() {
+		return empDOJ;
+	}
+
+	public void setEmpDOJ(Date empDOJ) {
+		this.empDOJ = empDOJ;
+	}
+
+	public String getLeaveBalance() {
+		return leaveBalance;
+	}
+
+	public void setLeaveBalance(String leaveBalance) {
+		this.leaveBalance = leaveBalance;
+	}
+
+	public Integer getEmpManagerId() {
+		return empManagerId;
+	}
+
+	public void setEmpManagerId(Integer empManagerId) {
+		this.empManagerId = empManagerId;
+	}
+}
