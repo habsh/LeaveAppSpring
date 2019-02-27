4bafb79f3153dfc82e33a566ad221f96befb7e82
Maxamillion000000000 <34814213+Maxamillion000000000@users.noreply.github.com>
2019-02-27 17:38:31 -0500

    added me a test unit

---
+++ src/main/webapp/src/test/testapp.java
@@ -0,0 +1,16 @@
+import junit.framework.TestCase;
+
+public class TestApp extends TestCase
+{
+        public void testID() {
+            public TestApp(String empMethod) {
+                super(empMethod);
+            }
+
+            public void testEmployee() {
+                Employee brad = new Employee(4892,"Brad Charleston", 7524200884, "Abigail@.com", "punk rulez","",58333,29344);
+                //how is Date represented in the string?
+                assertEquals("4892 Brad Charleston 7524200884 Abigail@.com punk rulez  58333 29344",brad.Employee());
+            }
+        }
+}
