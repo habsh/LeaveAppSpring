71801e7a56ac0b5c2513adc654c1686ee60fe1dc
Maxamillion000000000 <34814213+Maxamillion000000000@users.noreply.github.com>
2019-02-28 15:19:17 -0500

    drafted test

--- src/main/webapp/src/test/testapp.java
+++ src/main/webapp/src/test/testapp.java
@@ -13,4 +13,36 @@ public class TestApp extends TestCase
                 assertEquals("4892 Brad Charleston 7524200884 Abigail@.com punk rulez  58333 29344",brad.Employee());
             }
         }
-}
+
+        public void setUp() throws Excecption{
+            System.out.println("Setup - TestApp");
+        }
+
+        public void tearDown() throws Excecption{
+            System.out.println("Teardown - Testapp");
+        }
+
+        public static Test suite() {
+            TestSetup test =  new TestSetup(new TestSuite(TestApp.class))
+            {
+                public void setUp() throws Excecption{
+                    System.out.println("Setup suite - TestApp");
+                }
+                public void tearDown() throws Excecption {
+                    System.out.println("Teardown suite - TestApp");
+                }
+            };
+            return test;
+        }
+
+        public void testNulls() {
+            try{
+                System.out.println("from TestApp - testNulls");
+                Employee g = new Employee(null,null,null,null,null,null,null,null);
+                fail("Expected illegal arg exception when all args are null");
+            }
+            catch(Exception e){
+                System.out.println("Exception occurred"+e);
+            }
+        }
+ }
