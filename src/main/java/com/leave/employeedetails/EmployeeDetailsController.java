d2952a7ebf42c2406c875ae9bfbbd7545fa89721
abudri <17362519+abudri@users.noreply.github.com>
2019-03-04 16:50:05 -0500

    put the spring in boot with controllers

--- src/main/java/com/leave/employeedetails/EmployeeDetailsController.java
+++ src/main/java/com/leave/employeedetails/EmployeeDetailsController.java
@@ -1,8 +1,10 @@
-package com.leave;
+package com.leave.employeedetails;
 
 import org.springframework.web.bind.annotation.RestController;
 
 import com.leave.applyleave.service.ApplyLeaveService;
+import com.leave.dtos.EmployeeContactDetailsDTO;
+import com.leave.dtos.EmployeeDetailsDTO;
 import com.leave.obj.Leave;
 import com.leave.obj.LeaveErrors;
 import com.leave.obj.LeaveRequest;
@@ -15,9 +17,11 @@ import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @RestController
@@ -25,12 +29,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class EmployeeDetailsController {
 
 	@Autowired
-    EmployeeDetailsService employeeDetailsService;
+    EmployeeService employeeService;
 
     @RequestMapping(value="/details/{empId}", method = RequestMethod.GET)
     @ResponseBody
-    public EmployeeDetails employee(@RequestParam int empId) {
-        return details; 
+    public EmployeeContactDetailsDTO employee(@PathVariable("empId") int empId) {
+    	
+        return employeeService.getEmpDetails(empId); 
     }
+}
     
   
